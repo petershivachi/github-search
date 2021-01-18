05f56ff1198031d8cfeeb3629b22d0270c7a9eff
petershivachi <shitsukanepeter@gmailc.om>
2021-01-18 12:23:11 +0300

    fixed bugs in ui and github class

--- github.js
+++ github.js
@@ -9,7 +9,7 @@ class GitHub {
   async getUser(user){
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret${this.client_secret}`);
 
-    const reposResponse = await fetch(`https://api.github.com/users/repos${user}?per_page=${this.repos_count}&sort=${this.repos_sort}client_id=${this.client_id}&client_secret${this.client_secret}`);
+    const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
     
     const profile = await profileResponse.json();
 
